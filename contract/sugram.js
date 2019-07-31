d2a006f11c7dedf3f8447fa2d7d9dc144f562b90
just-anohaker <just.anohaker@gmail.com>
2019-07-31 14:42:09 +0800

    fixedme

--- contract/sugram.js
+++ contract/sugram.js
@@ -6,7 +6,7 @@ const bignum = require("bignumber");
 function isaddress(address) {
     if (typeof address !== "string") return false;
     if (!base58check.decodeUnsafe(address.slice(1))) return false;
-    if (["A"].indexOf(address[0]) !== -1) return false;
+    if (["A"].indexOf(address[0]) === -1) return false;
     return true;
 }
 
@@ -22,7 +22,7 @@ module.exports = {
             return "Invalid type " + type + ", supported types " + SUPPORTED_TYPES;
         }
 
-        if (!app.validate("amount", amount)
+        if (app.validate("amount", amount)
             || app.balances.get(this.trs.senderId, "ETM").lt(amount)) {
             return "Insufficient balance";
         }
